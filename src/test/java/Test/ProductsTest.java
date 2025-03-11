eaa4ff1876d0dbe4c5bf0566ccf3a33e48aef070
Joy baidya <joybaidya1137@gmail.com>
2025-03-12 02:28:35 +0600

    commiting Product test class

--- src/test/java/Test/ProductsTest.java
+++ src/test/java/Test/ProductsTest.java
@@ -1,6 +1,24 @@
 package Test;
 
+import PageClasses.ProductsPage;
 import Utils.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class ProductsTest extends BaseClass {
+
+
+    @Test(priority = 1)
+    public void testProductPageLoads(){
+         ProductsPage productsPage =new ProductsPage();
+        Assert.assertTrue(productsPage.isProductPageDisplayed(),"Products page did not load");
+    }
+    @Test(priority = 2)
+    public void testaddProductToCart(){
+        ProductsPage productsPage =new ProductsPage();
+        productsPage.addProductToCart();
+        Assert.assertTrue(productsPage.isProductPageDisplayed(),"product was not Add");
+    }
+
+
 }
