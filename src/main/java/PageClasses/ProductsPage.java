73d66ba192478fd983a92e27601c47a82e9a4175
Joy baidya <joybaidya1137@gmail.com>
2025-03-13 16:03:55 +0600

    commting product page  completed

--- src/main/java/PageClasses/ProductsPage.java
+++ src/main/java/PageClasses/ProductsPage.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
@@ -26,9 +28,9 @@ public class ProductsPage {
         return driver.findElement(By.className("inventory_list")).isDisplayed();
     }
 
-   // get List of all product names
+    // get List of all product names
     public List<WebElement> getAllProducts(){
-       return driver.findElements(By.className("inventory_item_name"));
+        return driver.findElements(By.className("inventory_item_name"));
     }
 
 
@@ -45,11 +47,55 @@ public class ProductsPage {
             System.out.println("Add to cart 2nd button clicked");
             addToCartButton.get(2).click();
             System.out.println("Add to cart 3rd button clicked");
-
         }
 
+    }
 
 
+       public void RemoveProducts() {
+           List<WebElement> RemoveButton = driver.findElements(By.xpath("//button[contains(text(),'Remove')]"));
+           if (!RemoveButton.isEmpty()) {
+               RemoveButton.get(0).click();
+               System.out.println("Remove button clicked");
+           }else {
+               System.out.println(" NO clicked Remove button");
+           }
+       }
+
+      public int getRemoveButtonCount() {
+         return driver.findElements(By.xpath("//button[contains(text(),'Remove')]")).size();
+       // Returns the count of remove buttons
+    }
+
+   // Select sorting option("Name (Z to A)")
+
+     public void selectSortingOption1(String option){
+        WebElement dropdown = driver.findElement(By.className("product_sort_container"));
+        dropdown.sendKeys(option);
+        System.out.println("Selected option:" +option);
+
+    }
+// oporer eselectSortingOption1 and niche selectSortingOption1 same kaj kore
+    public void selectSortingOption1() {
+        WebElement dropdown = driver.findElement(By.className("product_sort_container"));
+        Select select = new Select(dropdown);
+        select.selectByValue("hilo"); // Selecting sorting option by value and(" Price (high to low)")
+    }
+
+// dropDown  e productsTest ai method er jonno Loop Use korci
+public void selectSortingOption2(String optionText) {
+    WebElement dropdown = driver.findElement(By.className("product_sort_container"));
+    dropdown.sendKeys(optionText);
+    System.out.println("Selected option: " + optionText);
+}
+
+
+    //View the cart
+    public void viewCart(){
+    // WebElement cartButton = driver.findElement(By.xpath("//a[@class=\"shopping_cart_link\"]"));
+        WebElement cartButton = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]/a"));
+        cartButton.click();
+        System.out.println("Cart button clicked");
     }
 
 
