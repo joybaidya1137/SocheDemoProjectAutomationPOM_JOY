16f23231f07f6a866d675a55e9dda405e4cf29b6
Joy baidya <joybaidya1137@gmail.com>
2025-03-13 16:07:08 +0600

    commting CartTest.java t i am test 2 part

--- src/test/java/Test/CartTest.java
+++ src/test/java/Test/CartTest.java
@@ -1,6 +1,28 @@
 package Test;
 
+import PageClasses.CartPage;
 import Utils.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CartTest  extends BaseClass {
+    CartPage cartPage = new CartPage();
+
+
+    //cart pase loaded
+    @Test(priority = 1)
+    public void testCartPageLoads(){
+        Assert.assertTrue(cartPage.cartPageDisplayed(),"cart page did not load");
+        System.out.println("cart page load successfully");
+    }
+
+    @Test(priority = 2)
+    public void testCartItemCount() {
+       cartPage. printCartItemCount();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
