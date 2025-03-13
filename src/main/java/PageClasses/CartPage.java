ed72c46563c908bf3660f138af26bf60795be8cd
Joy baidya <joybaidya1137@gmail.com>
2025-03-13 16:06:15 +0600

    commting today ome update Work this  cart page

--- src/main/java/PageClasses/CartPage.java
+++ src/main/java/PageClasses/CartPage.java
@@ -1,14 +1,42 @@
 package PageClasses;
 
 import Utils.DriverManages;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class CartPage {
     private WebDriver driver;
 
     public CartPage(){
+
         this.driver= DriverManages.getDriver();
     }
+
+
+    //Check if cart Page is displayed
+    public boolean cartPageDisplayed(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cart_contents_container")));
+        return driver.findElement(By.id("cart_contents_container")).isDisplayed();
+    }
+
+    public void printCartItemCount() {
+        List<WebElement> cartNumber = driver.findElements(By.className("shopping_cart_badge"));
+        if (cartNumber.size() > 0) {
+            String count = cartNumber.get(0).getText();
+            System.out.println("Total items in cart: " + count);
+        } else {
+            System.out.println("Cart is empty.");
+        }
+    }
+
+
 
 
 }
